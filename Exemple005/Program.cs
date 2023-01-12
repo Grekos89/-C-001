1412187e622b3ca26edd855bfb0bf309aba3b937
Евгений <Grekos_89@mail.ru>
2023-01-12 16:17:31 +0300

    IfElse

---
+++ Exemple005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите ваше имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
